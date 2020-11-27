049cd08c8d205bdfd8ef26601674f2290e64484d
Quentin <qrichaud@gmail.com>
2020-11-27 12:00:08 +0100

    remove production files

---
+++ examples/BridgeExample2/src/com/company/AbstractMessageSender.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public abstract class AbstractMessageSender {
+    public abstract void SendMessage(String title, String details, int importance);
+}
+
---
+++ examples/ChainOfResponsibilityExample/src/com/company/AbstractRequestHandler.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public abstract class RequestHandler {
+    protected RequestHandler successor;
+
+    public void setSuccessor(RequestHandler successor) {
+        this.successor = successor;
+    }
+
+    public abstract void handleRequest(Request request);
+}
