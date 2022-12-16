9bf9240b38eda9430d821535fc841bcc3dd79d2b
Quentin Richaud <qrichaud@gmail.com>
2022-12-16 14:45:31 +0100

    Update singleton example

--- examples/SingletonExample/src/com/company/Main.java
+++ examples/SingletonExample/src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server();
 
+        /* Connect users Alice and Bob */
+        server.connect();
+
         /** An incoming request */
         Request incomingRequest = server.getIncomingRequest();
         RequestHandler handler = new RequestHandler();
