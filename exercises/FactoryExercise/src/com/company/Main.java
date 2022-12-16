e25fb2fccebe247043def18fc1a345d52e772691
Quentin Richaud <qrichaud@gmail.com>
2022-12-16 14:41:03 +0100

    Update factory exercise template

--- exercises/FactoryExercise/src/com/company/Main.java
+++ exercises/FactoryExercise/src/com/company/Main.java
@@ -1,33 +1,25 @@
 package com.company;
 
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
         /**
-         * For each coll to Server.getNewUserData(), you must parse the obtained data in order
+         * For each call to Server.getNewUserData(), you must parse the obtained data in order
          * to create an instance of AdminUser or CustomerUser. On this instance, you must then call
          * the method .saveToDatabase().
          *
          * Use the Factory Method Pattern in order to do so
          */
 
-        Server.getNewUserData();
 
+        for (int i = 0; i < 5; i++) {
+            Map<String, String> data = Server.getNewUserData();
 
+            // TODO
+        }
 
-        Server.getNewUserData();
-
-
-
-        Server.getNewUserData();
-
-
-
-        Server.getNewUserData();
-
-
-
-        Server.getNewUserData();
 
     }
 }
